fd968332237b787c22877f2d5c53d741c904f092
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2024-08-28 21:25:49 +0000

    feat: add pre-push git hook

--- devenv.nix
+++ devenv.nix
@@ -54,7 +54,7 @@
     pass_filenames = false;
   };
 
-  pre-commit.hooks.linting = {
+  pre-commit.hooks.pre-commit = {
     enable = true;
 
     # The name of the hook (appears on the report table):
@@ -62,6 +62,27 @@
 
     # The command to execute (mandatory):
     entry = "yarn lint-staged";
+
+    # The language of the hook - tells pre-commit
+    # how to install the hook (default: "system")
+    # see also https://pre-commit.com/#supported-languages
+    language = "node";
+
+    # Set this to false to not pass the changed files
+    # to the command (default: true):
+    pass_filenames = true;
+  };
+
+  pre-commit.hooks.pre-push = {
+    enable = true;
+
+    # The name of the hook (appears on the report table):
+    name = "Pre Push";
+
+    # The command to execute (mandatory):
+    entry = "yarn pre-push";
+
+    stages = [ "pre-push" ];
 
     # The language of the hook - tells pre-commit
     # how to install the hook (default: "system")
