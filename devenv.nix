5984849eab2c7951282d86817beb52fbc1d18c3b
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2024-08-28 21:04:15 +0000

    chore: update yarn to 1.22.22

--- devenv.nix
+++ devenv.nix
@@ -8,7 +8,7 @@
     pkgs.git
     pkgs.firebase-tools
     pkgs.nodejs_20
-    pkgs.yarn-berry
+    pkgs.yarn
     pkgs.nil
     pkgs.nixpkgs-fmt
   ];
@@ -54,5 +54,23 @@
     pass_filenames = false;
   };
 
+  pre-commit.hooks.linting = {
+    enable = true;
+
+    # The name of the hook (appears on the report table):
+    name = "Pre Commit";
+
+    # The command to execute (mandatory):
+    entry = "yarn lint-staged";
+
+    # The language of the hook - tells pre-commit
+    # how to install the hook (default: "system")
+    # see also https://pre-commit.com/#supported-languages
+    language = "node";
+
+    # Set this to false to not pass the changed files
+    # to the command (default: true):
+    pass_filenames = true;
+  };
   # See full reference at https://devenv.sh/reference/options/
 }
